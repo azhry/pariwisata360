8f443972b904d57248bc033431e90f6631c0ca1e
muhammad irsyad masyhudin <irsyadeinstein@gmail.com>
2018-04-10 20:54:07 +0700

    crud hak akses

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -128,6 +128,22 @@ class Admin extends MY_Controller {
 
 		// irsyad
 		// buat file baru hak_akses_data.php di ./views/admin
+		$this->load->model( 'Hak_akses_m' );
+		$this->data['action'] 	= $this->uri->segment( 4 );
+		if ( isset( $this->data['action'] ) && $this->data['action'] == 'delete' ) {
+
+			$this->data['id_hak_akses']	= $this->uri->segment( 3 );
+			$this->Hak_akses_m->delete( $this->data['id_hak_akses'] );
+			$this->flashmsg( '<i class="fa fa-check"></i> Data berhasil dihapus' );
+			redirect( 'admin/data-hak-akses' );
+			exit;
+
+		}
+
+		$this->data['akses']	= $this->Hak_akses_m->get();
+		$this->data['title']	= 'Data Hak Akses';
+		$this->data['content']	= 'admin/hak_akses_data';
+		$this->template( $this->data, 'admin' );
 
 	}
 
@@ -136,12 +152,55 @@ class Admin extends MY_Controller {
 		// irsyad
 		// buat file baru hak_akses_tambah.php di ./views/admin
 
+		if ( $this->POST( 'submit' ) ) {
+
+			$this->load->model( 'Hak_akses_m' );
+			$this->data['akses'] = [
+				'label' => $this->POST('label'),
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+			];
+			$this->Hak_akses_m->insert( $this->data['akses'] );
+			$this->flashmsg( '<i class="fa fa-check"></i> Data berhasil ditambahkan' );
+			redirect( 'admin/data-hak-akses' );
+			exit;
+
+		}
+
+		$this->data['title']		= 'Tambah Hak Akses';
+		$this->data['content']		= 'admin/hak_akses_tambah';
+		$this->template( $this->data, 'admin' );
 	}
 
 	public function edit_hak_akses() {
 
 		// irsyad
 		// buat file baru hak_akses_edit.php di ./views/admin
+		$this->data['id_hak_akses']	= $this->uri->segment( 3 );
+		$this->check_allowance( !isset( $this->data['id_hak_akses'] ) );
+
+		$this->load->model( 'hak_akses_m' );
+
+		if ( $this->POST( 'edit' ) ) {
+
+			$this->data['akses']	= [
+				'label' 		=> $this->POST( 'label' ),
+				'updated_at'	=> date( 'Y-m-d H:i:s' )
+			];
+			
+			$this->hak_akses_m->update( $this->data['id_hak_akses'], $this->data['akses'] );
+			$this->flashmsg( '<i class="fa fa-check"></i> Data berhasil di-edit' );
+			redirect( 'admin/edit-hak-akses/' . $this->data['id_hak_akses'] );
+			exit;
+
+		}
+
+		$this->data['akses']		= $this->hak_akses_m->get_row([ 'id_hak_akses' => $this->data['id_hak_akses'] ]);
+		$this->check_allowance( !isset( $this->data['akses'] ), [ '<i class="fa fa-warning"></i> Data not found', 'danger' ] );
+
+		$this->data['title']		= 'Edit Hak Akses';
+		$this->data['content']		= 'admin/hak_akses_edit';
+		$this->template( $this->data, 'admin' );
 
 	}
 
